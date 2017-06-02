d69edddfebf10e0a9ab821c1bc84bdf62ddc4728
Dimitrijs Fedotovs <dima@fedoto.ws>
2017-06-02 09:13:17 +0300

    cleanup

---
+++ src/main/java/javacourses/SayHelloServlet.java
@@ -0,0 +1,24 @@
+package javacourses;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/sayhello")
+public class SayHelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        PrintWriter writer = resp.getWriter();
+        writer.append("Hello, ").append(name).append("!");
+    }
+}
---
+++ src/main/webapp/hello.jsp
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>First WEB app</title>
+</head>
+<body>
+
+    <% for (int i = 0; i < 100; i++) { %>
+        <h1>Hello, World! <%= i + 1 %> </h1>
+    <% } %>
+
+
+</body>
+</html>
